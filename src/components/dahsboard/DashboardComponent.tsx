56ad6535a8b5b8a2005c0c4712c1a510935897c0
AxierPer <axierperlaz2018@gmail.com>
2024-11-28 16:55:48 -0500

    Add funtionallity,  to filter State or State ID or ZipCode + Hispanos percentage and population

--- src/components/dahsboard/DashboardComponent.tsx
+++ src/components/dahsboard/DashboardComponent.tsx
@@ -6,12 +6,26 @@ export default function DashboardComponent() {
   const [items, setItems] = useState<DataItem[]>([]);
   const [page, setPage] = useState<number>(1);
   const [totalPages, setTotalPages] = useState<number>(1);
-
   const [query, setQuery] = useState("")
+  const [hispanos, setHispanos] = useState("")
+  const [population, setPopulation] = useState("")
 
-  const filterPost = items.filter((zips) =>
-    zips.zipcode.toLowerCase().includes(query.toLowerCase()) ||
-    zips.state_name?.toLowerCase().includes(query.toLowerCase()))
+  const filterPrincipal = items.filter((zips) => {
+    const hispanosPercentage = zips.hispanos_in_us && zips.population
+      ? (zips.hispanos_in_us / zips.population) * 100
+      : 0;
+
+    const populationValue = zips.population || 0
+    return (
+      zips.zipcode.toLowerCase().includes(query.toLowerCase()) ||
+      zips.state_name?.toLowerCase().includes(query.toLowerCase()) ||
+      zips.state_id?.toLowerCase().includes(query.toLowerCase()) &&
+      (hispanos ? hispanosPercentage <= parseFloat(hispanos) : true) &&
+      (population ? populationValue <= parseInt(population, 10) : true)
+    )
+  }
+  )
+
 
   // Cargar los items desde la API
   const fetchItems = (page: number) => {
@@ -44,12 +58,29 @@ export default function DashboardComponent() {
   return (
     <>
       <div className="m-auto bg-zinc-400 p-2">
-        <input
-          className="w-2/12 bg-zinc-500 border-zinc-400 rounded p-1 mb-1 focus:ring-zinc-500 focus:ring-2 focus:outline-none"
-          placeholder="Search"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
+        <div className="flex h-12 mb-2">
+          <input
+            className="w-2/12 bg-zinc-500 border-zinc-400 mr-14 rounded p-1 mb-1 focus:ring-zinc-500 focus:ring-2 focus:outline-none"
+            placeholder="Search"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <div className="p-1">
+            <input
+              className="w-2/6 bg-zinc-500 border-zinc-400  rounded p-1 mb-1 focus:ring-zinc-500 focus:ring-2 focus:outline-none"
+              placeholder="Hispanos"
+              value={hispanos}
+              onChange={(e) => setHispanos(e.target.value)}
+            />
+            <span> to </span>
+            <input
+              className="w-2/6 bg-zinc-500 border-zinc-400  rounded p-1 mb-1 focus:ring-zinc-500 focus:ring-2 focus:outline-none"
+              placeholder="Population"
+              value={population}
+              onChange={(e) => setPopulation(e.target.value)}
+            />
+          </div>
+        </div>
         <div className="bg-zinc-500 p-2 rounded-lg">
           <div className="overflow-y-auto max-h-[750px] table-container m-auto">
             <table className="min-w-full h-full table-auto">
@@ -66,15 +97,15 @@ export default function DashboardComponent() {
                 </tr>
               </thead>
               <tbody>
-                {filterPost.map((item, index) => (
+                {filterPrincipal.map((item, index) => (
                   <tr key={`${item.zipcode} - ${index}`} className={`${index % 2 === 0 ? "bg-zinc-500" : "bg-zinc-300"} border-b border-gray-200`}>
                     <td className="px-4 py-2">{item.zipcode}</td>
                     <td className="px-4 py-2">
                       {item.hispanos_in_us && item.population
                         ? ((item.hispanos_in_us / item.population) * 100).toFixed(2)
-                        : 0}
+                        : "0.00"}
                       %</td>
-                    <td className="px-4 py-2">{item.population}</td>
+                    <td className="px-4 py-2">{new Intl.NumberFormat('de-DE').format(item.population ? item.population : 0)}</td>
                     <td className="px-4 py-2">{item.state_name}</td>
                     <td className="px-4 py-2">{item.state_id}</td>
                     <td className="px-4 py-2">{item.latitude}</td>
